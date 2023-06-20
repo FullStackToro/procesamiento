612d35446da3ec6d638f39dade724f002e61e674
Lotar <lotar.94@gmail.com>
2023-06-19 22:46:51 -0400

    se agrega maqueta resumen facturacion

--- front/src/app/views/billing-summary/billing-summary.component.ts
+++ front/src/app/views/billing-summary/billing-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-billing-summary',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./billing-summary.component.scss']
 })
 export class BillingSummaryComponent {
+
+  constructor(private readonly router:Router){}
+
+  openCity(evt:any, cityName:any) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      // tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  logout() {
+    this.router.navigate(['login'])
+  }
 
 }
