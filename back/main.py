648e847b95fa4bf2f3986d86dee00d10ee10bedd
Lotar <lotar.94@gmail.com>
2023-06-21 23:35:34 -0400

    Se crea base de datos para usuarios

--- back/main.py
+++ back/main.py
@@ -1,13 +1,7 @@
-from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    ctaContrato: int
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return "Hi, I am FastAPI "
---
+++ back/model/handle_db.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+class HandleDB():
+    def __init__(self):
+        self._con = sqlite3.connect("./users.db")
+        self._cur = self._con.cursor()
+    
+    def get_all(self):
+        data = self._cur.execute("SELECT * FROM users")
+        return data.fetchall()
+        
+    def get_only(self, data_user):
+        data = self._cur.execute("SELECT * FROM users WHERE username = '{}'".format(data_user))
+        return data.fetchone()
+    
+    def insert(self, data_user):
+        self._cur.execute("INSERT INTO users VALUES('{}','{}','{}','{}','{}')".format(
+            data_user["id"],
+            data_user["firstname"],
+            data_user["lastname"],
+            data_user["username"],
+            data_user["password_user"]
+        ))
+        self._con.commit()
+
+    def delete(self, data_id):
+        self._cur.execute("DELETE FROM users WHERE username = '{}'".format(data_id))
+
+    def __del__(self):
+        self._con.close()
+
+db = HandleDB()
+data = {
+    "id":1,
+    "firstname": "Gregory",
+    "lastname": "Vicent",
+    "username": "dudadedu",
+    "password_user": "12345"
+}
+#db.insert(data)
+
+print(db.get_all())
